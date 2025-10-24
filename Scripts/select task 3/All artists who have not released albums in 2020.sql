66f3f6f486e222a2ebde07c9a9bd9d0998030ef8
ReZero <88268775+barna7en@users.noreply.github.com>
2025-10-24 13:45:35 +0500

    Delete Scripts/select task 3 directory

--- Scripts/select task 3/All artists who have not released albums in 2020.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT art.artist_name
-FROM artists art
-WHERE art.artist_id NOT IN (
-    SELECT DISTINCT aa.artist_id
-    FROM album_artists aa
-    JOIN albums alb ON aa.album_id = alb.album_id
-    WHERE alb.release_year = 2020
-);
--- Scripts/select task 3/The number of performers in each genre.sql
+++
@@ -1,4 +0,0 @@
-SELECT g.genre_name, COUNT(DISTINCT ag.artist_id) as number_of_artists
-FROM genres g
-LEFT JOIN artist_genres ag ON g.genre_id = ag.genre_id
-GROUP BY g.genre_name;
